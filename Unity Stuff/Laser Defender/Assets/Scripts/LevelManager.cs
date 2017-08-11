df999dd7ac1463d0e231c33b6f2eb119a9c95afd
Julius Kukla <JuliusNEW@Julius-Kuklas-MacBook-Pro-2.local>
2017-08-11 18:00:49 -0400

    Worked on Laser Defender, 4:00pm, 8-11-17

--- Unity Stuff/Laser Defender/Assets/Scripts/LevelManager.cs
+++ Unity Stuff/Laser Defender/Assets/Scripts/LevelManager.cs
@@ -5,27 +5,35 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour {
 
 	public GameObject smallShip;
+	public Sprite shipDefaultSprite;
+	public AnimationClip flyBy;
+
 	GameObject smallShipClone;
 	static Scene currentScene;
 
 	void Start () {
-		smallShip.GetComponent<SpriteRenderer> ().sprite = ShipContoller.playerShipSprite;
-		currentScene = SceneManager.GetActiveScene ();
-		ShipTravel ();
+		StartCoroutine (WaitToLoad (5f));
 	}
 
 	void ShipTravel () {
+		Debug.Log ("ShipTravel is running");
 		if (currentScene.name == "Start Menu" || currentScene.name == "Settings" || currentScene.name == "Win Screen") { 
-			Vector3 smallShipPosition = new Vector3 (Random.Range (10f, 780f), smallShip.transform.position.y, smallShip.transform.position.z);
-			smallShipClone = Instantiate (smallShip, smallShipPosition, Quaternion.identity) as GameObject;
-			Destroy (smallShipClone, 25f);
-			StartCoroutine (WaitToLoad (30f));
-			return;
+			Debug.Log ("If statement is running");
+			smallShipClone = Instantiate (smallShip, smallShip.transform.position, Quaternion.identity) as GameObject;
+			Destroy (smallShipClone, 30f);
+			StartCoroutine (WaitToLoad (40f));
 		}
 	}
 
 	IEnumerator WaitToLoad (float waitTime) {
 		yield return new WaitForSecondsRealtime (waitTime);
+		Debug.Log ("Time Enumerator is running");
+		currentScene = SceneManager.GetActiveScene ();
+		if (Settings.playerShip != 1) {
+			smallShip.GetComponent<SpriteRenderer> ().sprite = ShipContoller.playerShipSprite;	
+		}
+
+		ShipTravel();
 	}
 
 	public void LoadLevel(string name){
