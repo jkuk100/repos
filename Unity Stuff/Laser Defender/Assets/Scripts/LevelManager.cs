3a0ecd500ae3bc557f63db7698bbf1f02b9e430e
Julius Kukla <JuliusNEW@Julius-Kuklas-MacBook-Pro-2.local>
2017-08-11 10:23:50 -0400

    Worked on small Ship generator in Laser Defender, 10:23am, 8-11-17

--- Unity Stuff/Laser Defender/Assets/Scripts/LevelManager.cs
+++ Unity Stuff/Laser Defender/Assets/Scripts/LevelManager.cs
@@ -4,6 +4,29 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
+	public GameObject smallShip;
+	GameObject smallShipClone;
+	static Scene currentScene;
+
+	void Start () {
+		smallShip.GetComponent<SpriteRenderer> ().sprite = ShipContoller.playerShipSprite;
+		currentScene = SceneManager.GetActiveScene ();
+		ShipTravel ();
+	}
+
+	void ShipTravel () {
+		if (currentScene.name == "Start Menu" || currentScene.name == "Settings" || currentScene.name == "Win Screen") { 
+			Vector3 smallShipPosition = new Vector3 (Random.Range (10f, 780f), smallShip.transform.position.y, smallShip.transform.position.z);
+			smallShipClone = Instantiate (smallShip, smallShipPosition, Quaternion.identity) as GameObject;
+			Destroy (smallShipClone, 25f);
+			StartCoroutine (WaitToLoad (30f));
+			return;
+		}
+	}
+
+	IEnumerator WaitToLoad (float waitTime) {
+		yield return new WaitForSecondsRealtime (waitTime);
+	}
 
 	public void LoadLevel(string name){
 		SceneManager.LoadScene (name);
