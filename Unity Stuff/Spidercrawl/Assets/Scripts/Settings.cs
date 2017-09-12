4ace27e25f7b9b49e349f0c5e611fa1564807072
Julius Kukla <JuliusNEW@Julius-Kuklas-MacBook-Pro-2.local>
2017-09-11 21:30:12 -0400

    Committing Spidercrawl, 9:30, 9-11-17

---
+++ Unity Stuff/Spidercrawl/Assets/Scripts/Settings.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour {
+	public InputField name1;
+	public InputField name2;
+	public InputField name3;
+	public InputField name4;
+
+	public Text player1name;
+	public Text player2name;
+	public Text player3name;
+	public Text player4name;
+
+
+
+	public void Player1Name () {
+		player1name.text = name1.GetComponent<InputField> ().text;
+	}
+
+	public void Player2Name () {
+		player2name.text = name2.GetComponent<InputField> ().text;
+	}
+
+	public void Player3Name () {
+		player3name.text = name3.GetComponent<InputField> ().text;
+	}
+
+	public void Player4Name () {
+		player4name.text = name4.GetComponent<InputField> ().text;
+	}
+
+
+}
